75edd1d812f518b5939f32afcbe3ce4c404791b9
AlexisRqs <alexisrqs@outlook.fr>
2025-09-14 16:56:31 +0200

    renamed RecordsMenu + fix

--- Scripts/UI/MainMenu.gd
+++ Scripts/UI/MainMenu.gd
@@ -3,8 +3,8 @@ extends Control
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Main/Main.tscn")
 
-func _on_records_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/UI/Records.tscn")
+func _on_records_pressed():
+	get_tree().change_scene_to_file("res://Scenes/UI/RecordsMenu.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
