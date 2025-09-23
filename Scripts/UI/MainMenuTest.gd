e6ffc7603cb0c3626b875aae05a2361cb8275e64
AlexisRqs <alexisrqs@outlook.fr>
2025-09-23 19:24:10 +0200

    testing UI nodes

---
+++ Scripts/UI/MainMenuTest.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Main/Main.tscn")
+	
+func _on_upgrades_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/UI/RecordsMenu.tscn")
+	
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/UI/OptionsMenu.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
