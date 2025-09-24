5150304117d3bad9075f35091e3b906758417b50
AlexisRqs <alexisrqs@outlook.fr>
2025-09-24 10:50:46 +0200

    Improved UI

--- Scripts/UI/MainMenuTest.gd
+++ Scripts/UI/MainMenuTest.gd
@@ -7,7 +7,7 @@ func _on_upgrades_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/UI/RecordsMenu.tscn")
 	
 func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/UI/OptionsMenu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/UI/OptionsMenuTest.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
