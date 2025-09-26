746a12d9a82874a26faa08db7150c9e1361c2af8
AlexisRqs <alexisrqs@outlook.fr>
2025-09-26 02:28:20 +0200

    Game version isn't hardcoded anymore

--- Scripts/UI/MainMenu.gd
+++ Scripts/UI/MainMenu.gd
@@ -5,6 +5,10 @@ const RECORDS_SCENE := preload("res://Scenes/UI/RecordsMenu.tscn")
 const OPTIONS_SCENE := preload("res://Scenes/UI/OptionsMenu.tscn")
 
 @onready var play_button = $MarginContainer/VBoxContainer/Center/PlayButton
+@onready var version_label: Label = $MarginContainer/VBoxContainer/Footer/VersionLabel
+
+func _ready() -> void:
+	version_label.text = "v" + str(ProjectSettings.get_setting("application/config/version"))
 
 func _on_play_pressed() -> void:
 	SceneLoader.go_to(MAIN_SCENE, SceneLoader.Direction.DOWN)
